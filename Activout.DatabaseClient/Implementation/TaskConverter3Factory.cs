4684e7225b6ade0838550f19bf93807e9144748d
David Eriksson <david@activout.se>
2025-03-03 20:08:01 +0100

    dotnet format

--- Activout.DatabaseClient/Implementation/TaskConverter3Factory.cs
+++ Activout.DatabaseClient/Implementation/TaskConverter3Factory.cs
@@ -6,7 +6,7 @@ public class TaskConverter3Factory : ITaskConverterFactory
 {
     public ITaskConverter CreateTaskConverter(Type actualReturnType)
     {
-        return actualReturnType == typeof(void) ? null : 
-            (ITaskConverter) Activator.CreateInstance(typeof(TaskConverter3<>).MakeGenericType(actualReturnType));
+        return actualReturnType == typeof(void) ? null :
+            (ITaskConverter)Activator.CreateInstance(typeof(TaskConverter3<>).MakeGenericType(actualReturnType));
     }
 }
